892a818475be9fae38f69c042f0f33793425bf81
Seth Westphal <seth.westphal@chctelehealth.com>
2016-09-28 09:38:46 -0500

    Remove some unnecessary packages.

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,11 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel', // 'babel-loader' is also a legal name to reference
         query: {
-          presets: ['react', 'es2015']
+          presets: ['es2015']
         }
       },
       {
